adc0dcdafd0a2327a0809e2e98ba500dafc0f7b4
Josh <jleblanc@omics.com>
2021-04-06 13:35:54 -0300

    enable legacy connection handling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.active_record.legacy_connection_handling = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
